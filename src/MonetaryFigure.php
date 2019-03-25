e7982e44d9c18319f586c9518bf9177487555524
Alphametric <admin@alphametric.co>
2019-03-25 13:32:26 +0000

    Added MonetaryFigure rule

---
+++ src/MonetaryFigure.php
@@ -0,0 +1,58 @@
+<?php
+
+// Namespace
+namespace Alphametric\Validation\Rules;
+
+// Monetary figure rule
+class MonetaryFigure extends Rule
+{
+
+	/**
+	 * Generate an example value that satisifies the validation rule.
+	 *
+	 * @param none.
+	 * @return string.
+	 *
+	 **/
+	public function example()
+	{
+		return $this->parameters[0] .
+			   mt_rand(1, (int) str_repeat('9', $this->parameters[1])) . '.' .
+			   mt_rand(1, (int) str_repeat('9', $this->parameters[2]));
+	}
+
+
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param string $attribute.
+     * @param mixed $value.
+     * @return bool.
+     *
+     **/
+    public function passes($attribute, $value)
+    {
+        return preg_match(
+            "/^\\{$this->parameters[0]}[0-9]{1,{$this->parameters[1]}}(\.[0-9]{1,{$this->parameters[2]}})?$/", $value
+        );
+    }
+
+
+
+    /**
+     * Get the validation error message.
+     *
+     * @param none.
+     * @return string.
+     *
+     **/
+    public function message()
+    {
+        return Helper::getLocalizedErrorMessage(
+            'monetary_figure',
+            'The :attribute must be a monetary figure e.g. ' . $this->example()
+        );
+    }
+
+}
---
+++ tests/MonetaryFigureTest.php
@@ -0,0 +1,49 @@
+<?php
+
+// Namespace
+namespace Alphametric\Validation\Rules\Tests;
+
+// Using directives
+use Orchestra\Testbench\TestCase as Orchestra;
+use Alphametric\Validation\Rules\MonetaryFigure;
+
+// Monetary figure test
+class MonetaryFigureTest extends Orchestra
+{
+
+	/** @test */
+	public function a_monetary_figure_can_be_validated()
+	{
+		// Define the validation rule
+		$rule = ['deposit' => [new MonetaryFigure('$', 4, 4)]];
+
+		// Execute the tests
+		$this->assertFalse(validator(['deposit' => '$'], $rule)->passes());
+		$this->assertTrue(validator(['deposit' => '$1'], $rule)->passes());
+		$this->assertTrue(validator(['deposit' => '$123'], $rule)->passes());
+		$this->assertTrue(validator(['deposit' => '$1234'], $rule)->passes());
+		$this->assertFalse(validator(['deposit' => '$12345'], $rule)->passes());
+		$this->assertFalse(validator(['deposit' => '$1234.'], $rule)->passes());
+		$this->assertTrue(validator(['deposit' => '$1234.1'], $rule)->passes());
+		$this->assertTrue(validator(['deposit' => '$1234.12'], $rule)->passes());
+		$this->assertTrue(validator(['deposit' => '$1234.123'], $rule)->passes());
+		$this->assertTrue(validator(['deposit' => '$1234.1234'], $rule)->passes());
+		$this->assertFalse(validator(['deposit' => '$1234.12345'], $rule)->passes());
+		$this->assertFalse(validator(['deposit' => '$12345.1234'], $rule)->passes());
+		$this->assertFalse(validator(['deposit' => '$abcd.efgh'], $rule)->passes());
+		$this->assertFalse(validator(['deposit' => '£1234.1234'], $rule)->passes());
+	}
+
+
+
+	/** @test */
+	public function a_monetary_figure_example_is_valid()
+	{
+		// Define the validation rule
+		$rule = ['deposit' => [$class = new MonetaryFigure('$', 4, 2)]];
+
+		// Execute the tests
+		$this->assertTrue(validator(['deposit' => $class -> example()], $rule)->passes());
+	}
+
+}
