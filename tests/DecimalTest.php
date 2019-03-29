e61e081bd54a4290ca9ec2a425681abeda85ef39
Alphametric <admin@alphametric.co>
2019-03-29 09:35:21 +0000

    Added Decimal rule

---
+++ tests/DecimalTest.php
@@ -0,0 +1,44 @@
+<?php
+
+// Namespace
+namespace Alphametric\Validation\Rules\Tests;
+
+// Using directives
+use Alphametric\Validation\Rules\Decimal;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+// Decimal test
+class DecimalTest extends Orchestra
+{
+
+	/** @test */
+	public function a_decimal_can_be_validated()
+	{
+		// Define the validation rule
+		$rule = ['figure' => [new Decimal(4, 2)]];
+
+		// Execute the tests
+		$this->assertFalse(validator(['figure' => '1'], $rule)->passes());
+		$this->assertFalse(validator(['figure' => '123'], $rule)->passes());
+		$this->assertFalse(validator(['figure' => '1234'], $rule)->passes());
+		$this->assertFalse(validator(['figure' => '1234.'], $rule)->passes());
+		$this->assertTrue(validator(['figure' => '1234.5'], $rule)->passes());
+		$this->assertTrue(validator(['figure' => '1234.56'], $rule)->passes());
+		$this->assertFalse(validator(['figure' => '1234.567'], $rule)->passes());
+		$this->assertTrue(validator(['figure' => '1234.0'], $rule)->passes());
+		$this->assertTrue(validator(['figure' => '1234.00'], $rule)->passes());
+	}
+
+
+
+	/** @test */
+	public function a_decimal_example_is_valid()
+	{
+		// Define the validation rule
+		$rule = ['figure' => [$class = new Decimal(4, 2)]];
+
+		// Execute the tests
+		$this->assertTrue(validator(['figure' => $class -> example()], $rule)->passes());
+	}
+
+}
