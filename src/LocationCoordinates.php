af62b8601c28b69cb1c7d62a7118dd9289b81ad8
Alphametric <admin@alphametric.co>
2019-04-02 14:08:44 +0100

    Added LocationCoordinates rule

---
+++ src/LocationCoordinates.php
@@ -0,0 +1,46 @@
+<?php
+
+// Namespace
+namespace Alphametric\Validation\Rules;
+
+// Location coordinates rule
+class LocationCoordinates extends Rule
+{
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * The latitude and longitude may have a maximum of
+	 * eight digits after the decimal point. This provides
+	 * an accuracy of up to ~1 millimeter.
+     *
+     * @param string $attribute.
+     * @param mixed $value.
+     * @return bool.
+     *
+     **/
+    public function passes($attribute, $value)
+    {
+        return preg_match(
+			"/^[-]?((([0-8]?[0-9])(\.(\d{1,8}))?)|(90(\.0+)?)),\s?[-]?((((1[0-7][0-9])|([0-9]?[0-9]))(\.(\d{1,8}))?)|180(\.0+)?)$/", $value
+		);
+    }
+
+
+
+    /**
+     * Get the validation error message.
+     *
+     * @param none.
+     * @return string.
+     *
+     **/
+    public function message()
+    {
+        return Helper::getLocalizedErrorMessage(
+            'location_coordinates',
+            'The :attribute must be a valid set of latitude and longitude coordinates'
+        );
+    }
+
+}
---
+++ tests/LocationCoordinatesTest.php
@@ -0,0 +1,34 @@
+<?php
+
+// Namespace
+namespace Alphametric\Validation\Rules\Tests;
+
+// Using directives
+use Orchestra\Testbench\TestCase as Orchestra;
+use Alphametric\Validation\Rules\LocationCoordinates;
+
+// Location coordinates test
+class LocationCoordinatesTest extends Orchestra
+{
+
+	/** @test */
+	public function a_set_of_location_coordinates_can_be_validated()
+	{
+		// Define the validation rule
+		$rule = ['location' => [new LocationCoordinates]];
+
+		// Execute the tests
+		$this->assertFalse(validator(['location' => '1'], $rule)->passes());
+		$this->assertTrue(validator(['location' => '1, 1'], $rule)->passes());
+		$this->assertTrue(validator(['location' => '1.0,1.0'], $rule)->passes());
+		$this->assertTrue(validator(['location' => '90.0,180.0'], $rule)->passes());
+		$this->assertFalse(validator(['location' => '90.0,181.0'], $rule)->passes());
+		$this->assertFalse(validator(['location' => '91.0,180.0'], $rule)->passes());
+		$this->assertTrue(validator(['location' => '-77.0364335, 38.8951555'], $rule)->passes());
+		$this->assertTrue(validator(['location' => '-77.03643357, 38.8951555'], $rule)->passes());
+		$this->assertTrue(validator(['location' => '-77.0364335, 38.89515557'], $rule)->passes());
+		$this->assertFalse(validator(['location' => '-77.036433576, 38.8951555'], $rule)->passes());
+		$this->assertFalse(validator(['location' => '-77.0364335, 38.895155576'], $rule)->passes());
+	}
+
+}
