38e09e7741ee7cd411e954e4f65bcfc14b29d6d8
Alphametric <33156775+alphametric@users.noreply.github.com>
2019-07-31 16:31:48 +0100

    Update DomainTest.php
    Added tests to handle domain edge cases.

--- tests/DomainTest.php
+++ tests/DomainTest.php
@@ -28,6 +28,8 @@ class DomainTest extends Orchestra
 		$this->assertFalse(validator(['domain' => 'google.com/test'], $rule)->passes());
 		$this->assertFalse(validator(['domain' => 'www.google.com/test'], $rule)->passes());
 		$this->assertFalse(validator(['domain' => 'www.google.com/test/test'], $rule)->passes());
+		$this->assertFalse(validator(['domain' => 'google'], $rule)->passes());
+		$this->assertFalse(validator(['domain' => 1], $rule)->passes());
 		$this->assertTrue(validator(['domain' => 'google.com'], $rule)->passes());
 		$this->assertTrue(validator(['domain' => 'www.google.com'], $rule)->passes());
 	}
