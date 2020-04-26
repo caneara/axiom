7328f224542f508f17165b70cc2b38a990798c5a
Guilherme Assemany <guilhermeassemany@hotmail.com>
2020-04-26 14:26:39 -0300

    Added MaxWords validation rule

---
+++ src/Rules/MaxWords.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types = 1);
+
+namespace Axiom\Rules;
+
+use Axiom\Types\Rule;
+
+class MaxWords extends Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     **/
+    public function passes($attribute, $value) : bool
+    {
+        return count(preg_split('~[^\p{L}\p{N}\']+~u',$value)) <= $this->parameters[0];
+    }
+
+
+
+    /**
+     * Get the validation error message.
+     *
+     **/
+    public function message() : string
+    {
+        return $this->getLocalizedErrorMessage(
+            'max_words',
+            'The :attribute cannot be longer than ' . $this->parameters[0] . ' words.'
+        );
+    }
+
+}
