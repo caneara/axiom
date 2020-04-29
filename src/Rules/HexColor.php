f64fc212c2ef0b8d9887aa67471f8b1b26db6465
innoflash <innocentmazando@gmail.com>
2020-04-29 11:03:30 +0200

    added 3 and 8 digit hex codes

--- src/Rules/HexColor.php
+++ src/Rules/HexColor.php
@@ -6,18 +6,23 @@ use Axiom\Types\Rule;
 
 class HexColor extends Rule
 {
+    private array $pregMatchArray = [
+        4 => '/^#([a-fA-F0-9]{3})$/i',
+        7 => '/^#([a-fA-F0-9]{6})$/i',
+        9 => '/^#([a-fA-F0-9]{8})$/i',
+    ];
 
-    /**
-     * @inheritDoc
-     */
     public function passes($attribute, $value)
     {
-        return preg_match('/^#([a-fA-F0-9]{6})$/i', $value) > 0;
+        if (! is_string($value) || ! in_array(strlen($value), array_keys($this->pregMatchArray))) {
+            return false;
+        }
+
+        $match = preg_match($this->pregMatchArray[strlen($value)], $value);
+
+        return $match > 0;
     }
 
-    /**
-     * @inheritDoc
-     */
     public function message()
     {
         return $this->getLocalizedErrorMessage(
--- tests/HexColorTest.php
+++ tests/HexColorTest.php
@@ -13,9 +13,11 @@ class HexColorTest extends TestCase
         $rule = ['hex_color' => [new HexColor()]];
 
         $this->assertTrue(validator(['hex_color' => '#f0f0f0'], $rule)->passes());
-        $this->assertFalse(validator(['hex_color' => '#f0f0f0ff'], $rule)->passes());
+        $this->assertTrue(validator(['hex_color' => '#fff'], $rule)->passes());
+        $this->assertTrue(validator(['hex_color' => '#00ff0080'], $rule)->passes());
         $this->assertFalse(validator(['hex_color' => 'f0f0f0'], $rule)->passes());
         $this->assertFalse(validator(['hex_color' => 'rgb(0,0,255)'], $rule)->passes());
         $this->assertTrue(validator(['hex_color' => '#000000'], $rule)->passes());
+        $this->assertTrue(validator(['hex_color' => '#ssssss'], $rule)->passes());
     }
 }
