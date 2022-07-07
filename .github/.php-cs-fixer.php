f22592f05bc3cbfc1da599d549eece7c7727b0e8
Matt Kingshott <51963402+mattkingshott@users.noreply.github.com>
2022-07-07 10:37:30 +0100

    Update .php-cs-fixer.php

--- .github/.php-cs-fixer.php
+++ .github/.php-cs-fixer.php
@@ -1,43 +1,51 @@
 <?php declare(strict_types = 1);
 
 $finder = Symfony\Component\Finder\Finder::create()
-    ->notPath('bootstrap/*')
-    ->notPath('storage/*')
-    ->notPath('storage/*')
-    ->notPath('resources/view/mail/*')
-    ->in([__DIR__ . '/../src', __DIR__ . '/../tests'])
+    ->notPath(dirname(__DIR__, 1) . '/bootstrap/*')
+    ->notPath(dirname(__DIR__, 1) . '/storage/*')
+    ->notPath(dirname(__DIR__, 1) . '/vendor')
+    ->notPath(dirname(__DIR__, 1) . '/resources/view/mail/*')
+    ->in([
+        dirname(__DIR__, 1) . '/src',
+        dirname(__DIR__, 1) . '/tests',
+    ])
     ->name('*.php')
     ->notName('*.blade.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
-        '@PSR2' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sort_algorithm' => 'length'],
-        'no_unused_imports' => true,
+        '@PSR2'                             => true,
+        'array_syntax'                      => ['syntax' => 'short'],
+        'ordered_imports'                   => ['sort_algorithm' => 'length'],
+        'no_unused_imports'                 => true,
         'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline_array' => true,
-        'phpdoc_scalar' => true,
-        'unary_operator_spaces' => true,
-        'binary_operator_spaces' => [
-            'align_double_arrow' => true,
-            'align_equals' => true,
+        'trailing_comma_in_multiline'       => ['elements' => ['arrays']],
+        'phpdoc_scalar'                     => true,
+        'unary_operator_spaces'             => true,
+        'binary_operator_spaces'            => [
+            'operators' => ['=' => 'align', '=>' => 'align'],
         ],
         'blank_line_before_statement' => [
             'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
         ],
         'phpdoc_single_line_var_spacing' => true,
-        'phpdoc_var_without_name' => true,
+        'phpdoc_var_without_name'        => true,
+        'class_attributes_separation'    => [
+            'elements' => [
+               'method'   => 'one',
+               'property' => 'one',
+            ],
+        ],
         'method_argument_space' => [
-            'on_multiline' => 'ensure_fully_multiline',
+            'on_multiline'                     => 'ensure_fully_multiline',
             'keep_multiple_spaces_after_comma' => true,
         ],
-        'method_chaining_indentation' => true,
+        'method_chaining_indentation'        => true,
         'object_operator_without_whitespace' => true,
-        'no_superfluous_phpdoc_tags' => true,
-        'function_declaration' => [
+        'no_superfluous_phpdoc_tags'         => true,
+        'function_declaration'               => [
             'closure_function_spacing' => 'none',
         ],
     ])
